718ea176892b5d22f49f5ad85817a38099abf219
Torben Bruns <torben.bruns@pcg.io>
2025-05-19 12:15:46 +0200

    fix: rm unused workflow files, modified start.sh to start frontend only

--- start.sh
+++ start.sh
@@ -19,14 +19,16 @@ if [ $? -ne 0 ]; then
     exit $?
 fi
 
-cd ..
-. ./scripts/loadenv.sh
+npm run dev 
 
-echo ""
-echo "Starting backend"
-echo ""
-./.venv/bin/python -m quart run --port=50505 --host=127.0.0.1 --reload
-if [ $? -ne 0 ]; then
-    echo "Failed to start backend"
-    exit $?
-fi
+# cd ..
+# . ./scripts/loadenv.sh
+
+# echo ""
+# echo "Starting backend"
+# echo ""
+# ./.venv/bin/python -m quart run --port=50505 --host=127.0.0.1 --reload
+# if [ $? -ne 0 ]; then
+#     echo "Failed to start backend"
+#     exit $?
+# fi
